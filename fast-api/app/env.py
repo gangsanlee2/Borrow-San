836e7392baad7ff5785873d14ef0d653622f5d96
phayeon <phayeon1222@gmail.com>
2023-02-14 14:42:06 +0900

    encoding 에러 해결

--- fast-api/app/env.py
+++ fast-api/app/env.py
@@ -9,6 +9,6 @@ DATABASE = 'bsdb'
 CHARSET = 'utf8'
 
 DB_url = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}"
-engine = create_engine(DB_url, encoding="utf-8", echo=True)
+engine = create_engine(DB_url, echo=True)
 
 conn = pymysql.connect(host=HOSTNAME, port=PORT, user=USERNAME, password=PASSWORD, db=DATABASE, charset=CHARSET)
